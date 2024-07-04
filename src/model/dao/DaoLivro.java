b000d753da64a7af62d0fa4d0b3dfb5babb7c75c
Guilherme-OLemos <guilherme.oslemos@gmail.com>
2024-07-04 14:23:56 -0300

    Finalização do método adicionar na classe Livro

--- src/model/dao/DaoLivro.java
+++ src/model/dao/DaoLivro.java
@@ -7,11 +7,31 @@ public class DaoLivro {
     final public static int Crescimento_dao = 2;
 
     private static int numObjeto = 0;
-    private static Livro[] lista_livro = new Livro[Tamanho_dao];
+    private static Livro[] lista = new Livro[Tamanho_dao];
 
     public boolean adicionar(Livro novo){
         if(DaoLivro.numObjeto == Tamanho_dao){
             return false;
         }
+
+        if (DaoLivro.numObjeto == DaoLivro.lista.length){
+            Livro[] nova_lista = new Livro[DaoLivro.lista.length + Crescimento_dao];
+            for (int posicao = 0; posicao < DaoLivro.numObjeto; posicao++){
+                nova_lista[posicao] = DaoLivro.lista[posicao];
+            }
+            DaoLivro.lista = nova_lista;
+        }
+
+        DaoLivro.lista[DaoLivro.numObjeto] = novo;
+        DaoLivro.numObjeto++;
+//        Finalizar Classe Serializador
+//        Serializador.salvarObjetos();
+        return true;
     }
+
+//    Método remover
+//    Método obter
+//    Método qtdObjetos
+//    Método obterLista
+//    Método adicionarTodos
 }
