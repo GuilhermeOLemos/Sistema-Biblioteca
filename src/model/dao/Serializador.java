72e4699ecce5ffb90f027076e789fac7404ddba7
Guilherme-OLemos <guilherme.oslemos@gmail.com>
2024-07-09 16:53:53 -0300

    Criação da classe Serializador; Criação de métodos incompleta

--- src/model/dao/Serializador.java
+++ src/model/dao/Serializador.java
@@ -14,11 +14,18 @@ import model.Periodico;
 public class Serializador {
     public static void salvarObjetos() {
         try {
+            FileOutputStream fos = new FileOutputStream("objetos.data");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
 
+            oos.close();
         } catch (FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    public static void recuperarObjetos() {
+
     }
 }
