6932bea13f4bb01b9ac4ac75eb8594da9221824f
Guilherme-OLemos <guilherme.oslemos@gmail.com>
2024-07-20 22:14:50 -0300

    Update JanelaAbstrata.java

--- src/viewer/JanelaAbstrata.java
+++ src/viewer/JanelaAbstrata.java
@@ -6,22 +6,22 @@ import javax.swing.JOptionPane;
 import controller.CtrlAbstrato;
 
 abstract public class JanelaAbstrata extends JFrame {
-	
+
 	private CtrlAbstrato ctrl;
-	
+
 	public JanelaAbstrata(CtrlAbstrato c) {
 		super();
 		this.ctrl = c;
 	}
-	
+
 	public CtrlAbstrato getCtrl() {
 		return this.ctrl;
 	}
-	
+
 	public void fechar() {
 		this.setVisible(false);
 	}
-	
+
 	public void notificar(String texto) {
 		JOptionPane.showMessageDialog(null, texto);
 	}
